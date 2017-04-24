1e8e2107e013ff3518ef26da5cbeb792b617053c
ydaniv <maggotfish@gmail.com>
2017-04-24 11:51:22 +0300

    Use Folder component

--- src/components/galleries/index.js
+++ src/components/galleries/index.js
@@ -1,17 +1,23 @@
 import {h, Component} from 'preact';
+import Folder from '../folder';
 
 export default class Galleries extends Component {
     render (props, state) {
 
-        let images = (props.images || []).map((img, i) => {
-            return <img src={img.thumbnailLink} key={i.toString()}/>
+        let folders = (props.folders || []).map((folder, i) => {
+            return (
+                <Folder key={i.toString()}
+                        id={folder.id}
+                        name={folder.name}
+                        iconLink={folder.iconLink}/>
+            );
         });
 
         return (
             <div className="view" id="galleries">
                 <h1>Galleries</h1>
-                <div>
-                    {images}
+                <div className="view-list">
+                    {folders}
                 </div>
             </div>
         );
