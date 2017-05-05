4c9f33e0a7c11f21a5c8b666906d3453c3f9f47f
ydaniv <maggotfish@gmail.com>
2017-05-06 00:23:15 +0300

    Added AddGalleryModal to Galleries

--- src/components/galleries/index.js
+++ src/components/galleries/index.js
@@ -1,5 +1,6 @@
 import {h, Component} from 'preact';
 import Folder from '../folder';
+import AddGalleryModal from './add-gallery-modal';
 
 export default class Galleries extends Component {
     render (props, state) {
@@ -15,6 +16,9 @@ export default class Galleries extends Component {
 
         return (
             <div className="view" id="galleries">
+                <AddGalleryModal isOpen={this.props.isAddModalOpen}
+                                 close={this.props.closeAddModal}
+                                 addGallery={this.props.addGallery}/>
                 <h1>Galleries</h1>
                 <div className="view-list">
                     {folders}
